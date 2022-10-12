038215db17b5f200048f406db26bcfd642f4b24f
Lucas Rizzini <lucasrizzini@hotmail.com>
2022-10-12 18:19:58 -0300

    atualizando

---
+++ root/.config/fish/config.fish
@@ -0,0 +1,13 @@
+set fish_greeting;
+starship init fish | source
+set VIRTUAL_ENV_DISABLE_PROMPT "1";
+source ~/.profile;
+if test -d ~/.local/bin
+    if not contains -- ~/.local/bin $PATH
+        set -p PATH ~/.local/bin
+    end
+end
+alias ls='/usr/bin/exa -al --color=always --group-directories-first --icons';
+alias grep='/usr/bin/grep --color=auto';
+alias syadm="/usr/bin/sudo /usr/bin/yadm -Y /etc/yadm";
+alias cat="highlight -O ansi --force"
